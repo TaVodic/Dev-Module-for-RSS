1b8ca0bf40a6242e6d6fe3be7cfb679b3333ef43
Martin <240703@vutbr.cz>
2023-12-04 09:31:47 +0100

    PCB ready for review

--- LED_strip_dev/main/main.c
+++ LED_strip_dev/main/main.c
@@ -1,29 +1,32 @@
 // https://github.com/JSchaenzle/ESP32-NeoPixel-WS2812-RMT
 
-#define NUM_LEDS 9
+#define NUM_LEDS      9
 #define LED_STRIP_PIN 4
 
+#include <driver/adc.h>
 #include <stdio.h>
+
+#include "driver/gpio.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/timers.h"
 #include "ws2812_control.h"
-#include "driver/gpio.h"
-
 
 #define RED   0x00FF00
 #define GREEN 0xFF0000
 #define BLUE  0x0000FF
 
-void app_main(void)
-{
-  ws2812_control_init();
+void app_main(void) {
+  adc1_config_width(ADC_WIDTH_BIT_12);
+  adc1_config_channel_atten(ADC1_CHANNEL_0, ADC_ATTEN_DB_11);
+  
 
-  struct led_state new_state;
-  for (uint8_t i = 0; i < NUM_LEDS; i++)
+  while (1)
   {
-    new_state.leds[i] = RED;
+    int val = adc1_get_raw(ADC1_CHANNEL_0);
+    printf("Val: %d\n", val);
+    vTaskDelay(500 / portTICK_PERIOD_MS);
   }
-  ws2812_write_leds(new_state);  
+  
 }
 
 /*
@@ -34,7 +37,14 @@ GPIO 33 12V_DIS - let floating, do not set anything, when the line 12V will need
   gpio_set_level(BLINK_GPIO, 1);
 
 #### LED RMT ####
-GPIO 4 - 
+GPIO 4 -
+  ws2812_control_init();
 
+  struct led_state new_state;
+  for (uint8_t i = 0; i < NUM_LEDS; i++)
+  {
+    new_state.leds[i] = RED;
+  }
+  ws2812_write_leds(new_state);
 
 */
